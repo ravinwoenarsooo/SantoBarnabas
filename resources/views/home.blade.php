fc5f8ba41cbc7cfc078fa52c10723df1bc38be8c
Your Name <you@example.com>
2023-12-09 00:45:04 +0700

    sampe warta paroki

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,93 @@
 @extends('layouts.home')
 
 @section('content')
+    <section id="section-1">
+        <div class="bg-cover flex items-center justify-center h-screen relative" 
+            style="background-image: url('{{ asset('images/background-1.png') }}');
+                    background-position: center; 
+                    position: relative;">
+
+            <!-- Background image with overlay -->
+            <div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: black; opacity: 0.5;"></div>
+
+            <!-- Text content with animated effect -->
+            <div class="flex-col text-center font-montserrat" style="z-index: 1;">
+                <!-- Apply the animated-text class to the heading -->
+                <h1 class="text-white text-6xl font-bold mb-5 animated-text-fade">IN VERBO AUTEM TUO LAXABO RETE</h1>
+                <h2 class="text-white text-3xl mt-5 animated-text-fade">“Karena perintah-Mu akan kutebarkan jalaku”</h2>
+            </div>
+        </div>
+    </section>  
+
+    <section id="section-2">
+        <div class="bg-white h-screen relative">
+            <h1 class="text-black text-6xl pt-32 font-bold text-center ">Gereja Santo Barnabas</h1>
+            <div class="mx-auto w-full flex px-5 py-12 md:flex-row flex-col items-center">
+                <div class="xl:w-40rem w-40rem mb-10 md:mb-0">
+                    <img class="object-cover h-auto object-center rounded" 
+                    alt="hero" 
+                    src="{{ asset('images/gereja.png') }}"
+                    >
+                </div>
+                <div class="xl:flex-grow w-1/2 xl:pl-12 md:pl-8 flex flex-col md:items-start md:text-left items-center text-center">
+                    <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Our History
+                    </h1>
+                    <p class="mb-8 leading-relaxed sm:text-xl text-3xl">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut labore et 
+                        dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure
+                        dolor in reprehenderit in voluptate velit esse cillum
+                        dolore eu fugiat nulla pariatur. Excepteur sint
+                    </p>
+                    <div class="flex justify-center">
+                        <button class="inline-flex text-black font-bold bg-yellow-700 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-800 rounded text-lg">Selanjutnya</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> <!-- Mario was he-->
+
+    <section id="section-1">
+        <div class="bg-cover flex items-center justify-center h-screen relative" 
+            style="background-image: url('{{ asset('images/misa.png') }}');
+                    background-position: center; 
+                    position: relative;">
+
+            <!-- Background image with overlay -->
+            <div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: black; opacity: 0.5;"></div>
+
+            <!-- Text content with animated effect -->
+            <div class="flex-col text-center font-montserrat" style="z-index: 1;">
+                <!-- Apply the animated-text class to the heading -->
+                <h1 class="text-white text-6xl font-bold mb-5 animated-text-fade">Jadwal Misa</h1>
+                <h2 class="text-white text-3xl mt-5 animated-text-fade">“Karena perintah-Mu akan kutebarkan jalaku”</h2>
+            </div>
+        </div>
+    </section>  
+
+    <section id="section-4">
+        
+    </section>
+
+    <div style="height: 1000px;"></div>
+
+    <!-- Add the CSS styles for the animated text -->
+    <style>
+        @keyframes fade {
+            0%, 100% {
+                opacity: 0;
+            }
+            100% {
+                opacity: 1;
+            }
+        }
+
+        /* Apply the fade transition styles */
+        .animated-text-fade {
+            animation: fade 2s ; /* Adjust the animation duration */
+        }
+    </style>
+@endsection
+    
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -3,37 +3,51 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Include Montserrat font using a link tag -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap">
     <title>Paroki Santo Barnabas</title>
     @vite('resources/css/app.css')
 </head>
 <body>
-    <div class="bg-white" style="background-image: url('{{ asset('images/Rectangle.png') }}');
-              background-size: cover;
-              background-position: center;
-              height: 70px;">
-            <div class="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col" bis_skin_checked="1">
-                <a class="flex title-font font-medium items-center md:justify-start justify-center text-white">
-                    <img src="{{ asset('images/email.png') }}" alt="Email Logo" class="w-6 h-auto">
+    <header>
+        <div class="bg-white h-70 lg:h-100" 
+            style="background-image: url('{{ asset('images/Rectangle.png') }}');
+            background-size: contain;
+            background-position: center;"
+        >
+        <div class="container px-5 py-6 mx-auto flex flex-wrap items-center justify-center sm:flex-row flex-col">
+            <div class="flex items-center mb-2 sm:mb-0">
+                <a class="flex title-font font-medium items-center md:justify-start justify-center text-white mt-1 mr-2">
+                    <img src="{{ asset('images/email.png') }}" alt="Email Logo" class="w-6 h-auto"> 
                 </a>
-                <p class="text-sm text-white font-bold sm:ml-2 sm:mt-0 mt-4 mr-4">santobarnabas@gmail.com</p>
-                <a class="flex title-font font-medium items-center md:justify-start justify-center text-white">
-                    <img src="{{ asset('images/phone.png') }}" alt="Phone Logo" class="w-6 h-auto">
-                </a>
-                <p class="text-sm text-white font-bold sm:ml-2 sm:mt-0 mt-4">+62 123 4567 8901</p>
-                <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                    <a href="https://www.youtube.com/@KomsosBarnabas" class="ml-3 text-white">
-                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-auto" viewBox="0 0 24 24">
-                            <path d="M23.2 7.6s-.2-1.6-1-2.3c-.9-1-1.9-1-2.4-1.1C17.4 4 12 4 12 4s-5.4 0-7.8.2c-.5 0-1.5.1-2.4 1.1C.8 6 .8 7.6.8 7.6s-.2 1.7-.2 3.4v1.8c0 1.7.2 3.4.2 3.4s.2 1.6 1 2.3c.9 1 2 1 2.5 1.1 1.8.2 7.5.2 7.5.2s5.4 0 7.8-.2c.5 0 1.5-.1 2.4-1.1.7-.8 1-2.3 1-2.3s.2-1.7.2-3.4v-1.8c0-1.7-.2-3.4-.2-3.4zM9.5 15.8V8.8l6.7 3.5-6.7 3.5z"></path>
-                        </svg>
-                    </a>
-                    <a href="https://www.instagram.com/komsossantobarnabas/" class="ml-3 text-white">
-                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-auto" viewBox="0 0 24 24">
-                            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                        </svg>
-                    </a>
-                </span>
+                <p class="text-md text-white font-bold mr-4">santobarnabas@gmail.com</p>
             </div>
+            
+            <div class="flex items-center">
+                <a class="flex title-font font-medium items-center md:justify-start justify-center text-white mr-2">
+                    <img src="{{ asset('images/phone.png') }}" alt="Phone Logo" class="w-6 h-auto"> 
+                </a>
+                <p class="text-md text-white font-bold">+62 123 4567 8901</p>
+            </div>
+            
+            <span class="inline-flex sm:ml-auto justify-center sm:justify-start">
+                <a href="https://www.youtube.com/@KomsosBarnabas" class="ml-3 text-white"
+                onmouseover="this.style.color='red'" 
+                onmouseout="this.style.color='black'">
+                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-8 h-auto" viewBox="0 0 24 24">
+                        <path d="M23.2 7.6s-.2-1.6-1-2.3c-.9-1-1.9-1-2.4-1.1C17.4 4 12 4 12 4s-5.4 0-7.8.2c-.5 0-1.5.1-2.4 1.1C.8 6 .8 7.6.8 7.6s-.2 1.7-.2 3.4v1.8c0 1.7.2 3.4.2 3.4s.2 1.6 1 2.3c.9 1 2 1 2.5 1.1 1.8.2 7.5.2 7.5.2s5.4 0 7.8-.2c.5 0 1.5-.1 2.4-1.1.7-.8 1-2.3 1-2.3s.2-1.7.2-3.4v-1.8c0-1.7-.2-3.4-.2-3.4zM9.5 15.8V8.8l6.7 3.5-6.7 3.5z"></path>
+                    </svg>
+                </a>
+                <a href="https://www.instagram.com/komsossantobarnabas/" class="ml-3 text-white"
+                onmouseover="this.style.color='white'" 
+                onmouseout="this.style.color='black'">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-8 h-auto" viewBox="0 0 24 24">
+                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                    </svg>
+                </a>
+            </span>
+        </div>
         </div>
     <div class="w-full text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800">
         <div x-data="{ open: false }" class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
@@ -64,10 +78,11 @@
             </nav>
         </div>
     </div>
+    <header>
     <main class="py-0" style="background-color: #FAFBDF;">
         @yield('content')
     </main>
-    <footer class="text-gray-400 bg-stone-800 body-font fixed bottom-0 w-full">
+    <footer class="text-gray-400 bg-stone-800 body-font bottom-0 w-full">
         <div class="py-8 flex flex-wrap -mb-10 md:text-left text-white order-first md:w-1/2 w-full ml-4">
             <div class="md:w-1/2 w-full px-4">
                 <h1 class="sm:text-3xl text-2xl font-medium title-font text-left text-white mx-5 mb-20">
@@ -78,9 +93,9 @@
             </div>
             <div class="md:w-1/2 w-full px-4">    
             <a href="https://www.google.com/maps/place/Saint+Barnabas+Catholic+Church,+Pamulang/@-6.348557,106.7476011,17z/data=!3m1!4b1!4m16!1m9!4m8!1m0!1m6!1m2!1s0x2e69ef0c62f22ac9:0x7f8b5476db79fc14!2sSaint+Barnabas+Catholic+Church,+Pamulang,+Jl.+Moh.+Toha+No.3,+RT.002%2FRW.005,+Pondok+Cabe+Udik,+Pamulang,+South+Tangerang+City,+Banten+15418!2m2!1d106.7502397!2d-6.3485621!3m5!1s0x2e69ef0c62f22ac9:0x7f8b5476db79fc14!8m2!3d-6.3485623!4d106.750176!16s%2Fg%2F1pzq1l0n9?entry=ttu" target="_blank">
-                <button class="text-4xl font-bold mb-4">Lokasi Gereja</button>
+                <button class="text-4xl font-bold mb-4" style="text-decoration: none;" onmouseover="this.style.textDecoration='underline'" onmouseout="this.style.textDecoration='none'">Lokasi Gereja</button>
                 
-                <nav class="list-none mb-10">
+                <nav class="list-none mb-10" style="text-decoration: none;" onmouseover="this.style.textDecoration='underline'" onmouseout="this.style.textDecoration='none'">
                     St. Barnabas <br className='md:hidden block' />
                     Jl. Moh. Toha No.3, <br className='md:hidden block' />
                     RT.002/RW.005, Pd. Cabe Udik, <br className='md:hidden block' />
@@ -92,19 +107,23 @@
         <div class="bg-white" bis_skin_checked="1">
             <div class="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col" bis_skin_checked="1">
                 <a class="flex title-font font-medium items-center md:justify-start justify-center text-white">
-                    <img src="{{ asset('images/paroki.png') }}" alt="Paroki Logo" class="w-10 h-10 rounded-full">
+                    <img src="{{ asset('images/paroki.png') }}" alt="Paroki Logo" class="w-8 h-8 rounded-full">
                 </a>
-                <p class="text-sm text-black sm:ml-6 sm:mt-0 mt-4">&copy; {{ date('Y') }}</p>
-                <p class="text-sm text-black font-bold sm:ml-6 sm:mt-0 mt-4">Komsos St.Barnabas Pamulang</p>
-                <p class="text-sm text-black sm:ml-6 sm:mt-0 mt-4">All Right Reserved.</p>
+                <p class="text-md text-black sm:ml-6 sm:mt-0 mt-4">&copy; {{ date('Y') }}</p>
+                <p class="text-md text-black font-bold sm:ml-6 sm:mt-0 mt-4">Komsos St.Barnabas Pamulang</p>
+                <p class="text-md text-black sm:ml-6 sm:mt-0 mt-4">All Right Reserved.</p>
                 <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                    <a href="https://www.youtube.com/@KomsosBarnabas" class="ml-3 text-black">
-                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                    <a href="https://www.youtube.com/@KomsosBarnabas" class="ml-3 text-black"
+                    onmouseover="this.style.color='red'" 
+                    onmouseout="this.style.color='black'">
+                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-8 h-auto" viewBox="0 0 24 24">
                             <path d="M23.2 7.6s-.2-1.6-1-2.3c-.9-1-1.9-1-2.4-1.1C17.4 4 12 4 12 4s-5.4 0-7.8.2c-.5 0-1.5.1-2.4 1.1C.8 6 .8 7.6.8 7.6s-.2 1.7-.2 3.4v1.8c0 1.7.2 3.4.2 3.4s.2 1.6 1 2.3c.9 1 2 1 2.5 1.1 1.8.2 7.5.2 7.5.2s5.4 0 7.8-.2c.5 0 1.5-.1 2.4-1.1.7-.8 1-2.3 1-2.3s.2-1.7.2-3.4v-1.8c0-1.7-.2-3.4-.2-3.4zM9.5 15.8V8.8l6.7 3.5-6.7 3.5z"></path>
                         </svg>
                     </a>
-                    <a href="https://www.instagram.com/komsossantobarnabas/" class="ml-3 text-black">
-                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                    <a href="https://www.instagram.com/komsossantobarnabas/" class="ml-3 text-black"
+                    onmouseover="this.style.color='#962fbf'" 
+                    onmouseout="this.style.color='black'">
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-8 h-auto" viewBox="0 0 24 24">
                             <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                         </svg>
