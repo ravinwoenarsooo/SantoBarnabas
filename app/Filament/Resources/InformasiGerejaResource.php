4ef7731aec84e92b99aef82f85fe0b80c070666f
ravinwoenarsooo <127296688+ravinwoenarsooo@users.noreply.github.com>
2023-12-16 13:59:50 +0700

    s

--- app/Filament/Resources/InformasiGerejaResource.php
+++ app/Filament/Resources/InformasiGerejaResource.php
@@ -53,6 +53,7 @@ class InformasiGerejaResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
